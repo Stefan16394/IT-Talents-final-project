ed130f9f7c43626582a1a4d0700a07b79cc6f523
Stefan16394 <rangelov1994@gmail.com>
2019-03-02 12:54:44 +0200

    improved getSubcategories query

--- src/main/java/com/vmzone/demo/service/CategoryService.java
+++ src/main/java/com/vmzone/demo/service/CategoryService.java
@@ -16,33 +16,34 @@ import com.vmzone.demo.repository.CategoryRepository;
 public class CategoryService {
 	@Autowired
 	public CategoryRepository categoryRepository;
-	
+
 	public List<ListCategory> getAllMainCategories() {
-		List<ListCategory> mainCategories = this.categoryRepository.findAll()
-				.stream()
-				.filter(c->c.getParent() == null)
-				.map(c->this.getCategoryById(c.getCategoryId()))
+
+		List<ListCategory> mainCategories = this.categoryRepository.findAll().stream()
+				.filter(c -> c.getParent() == null).map(c -> new ListCategory(c.getCategoryId(), c.getName(),
+						c.getParent(), this.getSubcategoriesForCategory(c.getCategoryId())))
 				.collect(Collectors.toList());
 
 		return mainCategories;
 	}
-	
+
 	public ListCategory getCategoryById(long id) {
-		
-		ListCategory categ= this.categoryRepository.findById(id)
-				.map(c->new ListCategory(c.getCategoryId(), c.getName(), c.getParent(),
-						this.categoryRepository.findAll().stream()
-						.filter(cat-> cat.getParent()!=null && cat.getParent().getCategoryId().equals(c.getCategoryId()))
-						.map(subCat->new ListSubCategory(subCat.getCategoryId(),subCat.getName()))
-						.collect(Collectors.toList())
-				))
-						.get();
+
+		ListCategory categ = this.categoryRepository.findById(id).map(c -> new ListCategory(c.getCategoryId(),
+				c.getName(), c.getParent(), this.getSubcategoriesForCategory(c.getCategoryId()))).get();
 		return categ;
 	}
 
 	public void createCategory(AddCategoryDTO category) {
-		Category newCategory = new Category(category.getCategory_id(),category.getName(),
+		Category newCategory = new Category(category.getCategory_id(), category.getName(),
 				this.categoryRepository.findById(category.getParent_id()).get());
 		this.categoryRepository.save(newCategory);
+	}
+
+	public List<ListSubCategory> getSubcategoriesForCategory(long id) {
+		return this.categoryRepository.findAll().stream()
+				.filter(cat -> cat.getParent() != null && cat.getParent().getCategoryId().equals(id))
+				.map(subCat -> new ListSubCategory(subCat.getCategoryId(), subCat.getName()))
+				.collect(Collectors.toList());
 	}
 }
